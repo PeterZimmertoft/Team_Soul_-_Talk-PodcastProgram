769db2ec039c2701a477037f4892bf3677d6d476
Senan Salah <senansalah2@gmail.com>
2026-04-16 11:17:47 +0200

    Fået oprettet Visual studio projektmappen

---
+++ Soul&Talk/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Soul_Talk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
