06da90907814a8e09fb7f84c7d04a9ddbae8b921
Tore <toreyoghurtsen@gmail.com>
2026-05-13 15:08:15 +0200

    second unit test, IK SLET SENAN

--- Informationsartefakter/15 - Relationel Databaseskema.md
+++ Informationsartefakter/15 - Relationel Databaseskema.md
@@ -6,8 +6,6 @@ GUEST(<ins>GuestId</ins> Name, Phone, Email, ConsentStatus, *PodcastEpisodeId*, 
 CITIZEN(<ins>CitizenId</ins>, Name, CPRNumber, WorkStatus, WorkType, ConsentStatus, CurrentStatus, SpecialConsiderations, *CaseOfficerId*)
 
 
-LOCALAUTHORITY(<ins>LocalAuthorityId</ins>, LocalAuthorityName, EANNumber)
-
 CASEOFFICER(<ins>CaseOfficerId</ins>, Name, Department, Phone, Email, *LocalAuthorityId*)
 
 PODCASTEPISODE(<ins>PodcastId</ins>, Title, Date, Duration, Status, MeetingPlace, Note, *GuestId*, *CaseOfficerId*)
---
+++ TestProject1/Add_And_GetById_ShouldPersistAndReadGuest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Soul_Talk.Model;
+using Soul_Talk.Persistence__Repositories_;
+
+[TestClass]
+public class GuestRepositoryTests
+{
+    
+    private readonly string _connectionString = "Server=localhost;Database=SoulTalkDB;Trusted_Connection=True;TrustServerCertificate=True;";
+    private GuestRepository _repository;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repository = new GuestRepository(_connectionString);
+    }
+
+    [TestMethod]
+    public void Add_And_GetById_ShouldPersistAndReadGuest()
+    {
+        // Arrange
+
+        var testGuest = new Guest
+        {
+            Name = "Senan Bradley",
+            Phone = "1234567890",
+            Email = "Senan@Bradley.com",
+        };
+
+        // Act
+
+            int newGuestId = _repository.Add(testGuest);
+
+            Guest retrievedGuest = _repository.GetById(newGuestId);
+
+        // Assert
+
+            Assert.IsNotNull(retrievedGuest);
+            Assert.AreEqual(testGuest.Name, retrievedGuest.Name);
+            Assert.AreEqual(testGuest.Phone, retrievedGuest.Phone);
+            Assert.AreEqual(testGuest.Email, retrievedGuest.Email);
+    }
+}
